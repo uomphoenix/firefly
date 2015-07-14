1f5d963b2ed6b7252d82b2f2a670dc2d3af33b80
Prithu Parker <prithu@iinet.net.au>
2015-07-14 20:34:54 +1000

    Initial FrameCache implementation - can add frames!

--- cache.py
+++ cache.py
@@ -4,5 +4,32 @@ cache.py
 Provides methods and classes for maintaning and managing a video frame cache.
 """
 
+import logging
+import collections
+import time
+import threading
+
 class FrameCache(object):
-    pass
+    """
+    The cache will be accessed from multiple threads, therefore we need to
+    make it thread safe.
+    """
+    def __init__(self, size):
+        self.size = size
+        self._cache = collections.deque()
+
+        self.lock = threading.RLock()
+
+    def add_frame(self, frame):
+        with self.lock:
+            to_cache = (frame, time.time())
+
+            self._cache.add(to_cache)
+
+            # If the length of the cache is bigger than the allowed size, pop
+            # the first item
+            if len(self) > self.size:
+                self._cache.popleft()
+
+    def __len__(self):
+        return len(self.cache)
