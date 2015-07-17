f6f0de4f9457c9588d69820565a240b44bbfc2ec
Prithu Parker <prithu@iinet.net.au>
2015-07-17 21:03:23 +1000

    Base implementation of initialization & run

--- daemon.py
+++ daemon.py
@@ -23,6 +23,44 @@ from pprint import pprint
 
 import settings
 
+import authentication
+import receiver
+import relay
+import observer
 
 if __name__ == "__main__":
-    pass
+    authenticator = authentication.Authenticator()
+
+    auth_server_address = (
+        settings.authentication["host"], 
+        settings.authentication.port
+    )
+
+    auth_server = authentication.AuthenticationServer(auth_server_address, 
+        authenticator)
+    #relay_server = relay.Rela
+
+    receiver_server_address = (
+        settings.receiver.host,
+        settings.receiver.port
+    )
+
+    receiver_server = receiver.ReceiverServer(receiver_server_address, 
+        authenticator)
+
+    recv_thread = threading.Thread(target = receiver_server.serve_forever)
+    recv_thread.daemon = True
+
+    try:
+        recv_thread.start()
+
+        auth_server.serve_forever()
+
+    except KeyboardInterrupt:
+        # Exit cleanly
+        print "KeyboardInterrupt. Exiting"
+
+    except:
+        logging.exception("Unknown exception in main thread. Exiting")
+
+        quit()
