cf07dfbaa6bb1231fe10de35a5e0e5dece30a28d
Prithu Parker <prithu@iinet.net.au>
2015-08-02 18:22:27 +1000

    Moved manual auth and receive test

--- simpletest/test_authentication.py
+++ simpletest/test_authentication.py
@@ -16,3 +16,20 @@ data = s.recv(128)
 
 print "Received '%s' in response" % (repr(data),)
 print "Data as string: %s" % (data,)
+
+s.close()
+
+if len(data) > 0:
+    auth_code, challenge_token, host, port, tail = data.split("\x00")
+    port = int(port)
+
+    receiver_address = (host,port)
+    c = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    c.connect(receiver_address)
+
+    to_send = "%s\x00TEST_FRAME\x00" % challenge_token
+    print "Sending %s to %s" % (repr(to_send), receiver_address)
+    c.send(to_send)
+
+    c.close()
